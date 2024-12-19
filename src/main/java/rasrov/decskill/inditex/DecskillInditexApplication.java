eb1f6a41b32dfd3da0cbc0e3b9b3fde9e0b188e5
rasul <rasulakhmeddibirov@gmail.com>
2024-12-19 10:14:01 +0100

    Init project

---
+++ src/main/java/rasrov/decskill/inditex/DecskillInditexApplication.java
@@ -0,0 +1,13 @@
+package rasrov.decskill.inditex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecskillInditexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecskillInditexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rasrov/decskill/inditex/DecskillInditexApplicationTests.java
@@ -0,0 +1,13 @@
+package rasrov.decskill.inditex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecskillInditexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
