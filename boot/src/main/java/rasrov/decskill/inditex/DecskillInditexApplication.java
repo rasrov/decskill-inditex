9b6c121360902453af329055b4594d7407dc1401
rasul <rasulakhmeddibirov@gmail.com>
2024-12-19 10:55:54 +0100

    Implemented hexagonal

--- boot/src/main/java/rasrov/decskill/inditex/DecskillInditexApplication.java
+++ boot/src/main/java/rasrov/decskill/inditex/DecskillInditexApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DecskillInditexApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DecskillInditexApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DecskillInditexApplication.class, args);
+    }
 
 }
---
+++ domain/src/main/java/rasrov/decskill/inditex/Main.java
@@ -0,0 +1,7 @@
+package rasrov.decskill.inditex;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
