05e665825b75422cf227fdcdae4aa87ac381d2b6
rasul <rasulakhmeddibirov@gmail.com>
2024-12-22 13:20:33 +0100

    Implemented cache config

--- boot/src/main/java/rasrov/decskill/inditex/DecskillInditexApplication.java
+++ boot/src/main/java/rasrov/decskill/inditex/DecskillInditexApplication.java
@@ -2,8 +2,10 @@ package rasrov.decskill.inditex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class DecskillInditexApplication {
 
     public static void main(String[] args) {
---
+++ boot/src/main/java/rasrov/decskill/inditex/config/CacheConfig.java
@@ -0,0 +1,34 @@
+package rasrov.decskill.inditex.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.concurrent.TimeUnit;
+
+@ConfigurationProperties(prefix = "cache")
+@Validated
+public record CacheConfig(@NotNull Integer duration,
+                          @NotNull Integer maxSize) {
+
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(duration, TimeUnit.MINUTES)
+                .maximumSize(maxSize);
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(caffeine);
+        return cacheManager;
+    }
+
+}
